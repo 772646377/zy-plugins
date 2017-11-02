567ebf0063e11161f962145fcde759f14efb4217
赵宇 <zhaoyua@zbj.com>
2017-11-02 10:39:44 +0800

    工程初始化

---
+++ src/com/zy/plugins/geterseter/AddGeterSeterAnnotationAction.java
@@ -0,0 +1,17 @@
+package com.zy.plugins.geterseter;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+/**
+ * @author zhaoyu
+ * @description 添加get set方法注释
+ * @date 2017/11/1
+ */
+public class AddGeterSeterAnnotationAction extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent anActionEvent) {
+
+    }
+}
---
+++ src/com/zy/plugins/wordcast/CamelCastUnderLineAction.java
@@ -0,0 +1,16 @@
+package com.zy.plugins.wordcast;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+/**
+ * @author zhaoyu
+ * @description
+ * @date 2017/11/1
+ */
+public class CamelCastUnderLineAction extends AnAction {
+    @Override
+    public void actionPerformed(AnActionEvent anActionEvent) {
+
+    }
+}
